85b968dff2f66af017a31ae5736492a6ec3e0dd6
AndrelGanShiTV <andresfelipegiraldoandrade@gmail.com>
2026-07-23 15:13:49 -0500

    Correct: Entitie Project filled

---
+++ src/Portfolio.Domain/Entities/Project.cs
@@ -0,0 +1,16 @@
+namespace Portfolio.Domain.Entities;
+
+public class Project
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public string? RepositoryUrl { get; set; }
+
+    public string? DemoUrl { get; set; }
+
+    public bool IsPublished { get; set; }
+}
